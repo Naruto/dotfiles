d9000deb73608dd9070c67690edd92604129a655
Naruto TAKAHASHI <tnaruto@gmail.com>
2013-04-17 11:51:51 +0900

    add markdown mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -218,3 +218,10 @@
 
 (require 'rust-mode)
 
+;; markdown mode
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
