099c66d48a722fe24249c0c2fc78214968a379cd
Naruto TAKAHASHI <tnaruto@gmail.com>
2013-11-01 16:12:51 +0900

    add "linux-tab-only" c-style for coding linux kernel.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -179,6 +179,25 @@
                                    (statement-case-intro . 3)
                                    (inclass . 3)
                                    ))))
+
+  ;; kernel
+  (defun c-lineup-arglist-tabs-only (ignored)
+    "Line up argument lists by tabs, not spaces"
+    (let* ((anchor (c-langelem-pos c-syntactic-element))
+           (column (c-langelem-2nd-pos c-syntactic-element))
+           (offset (- (1+ column) anchor))
+           (steps (floor offset c-basic-offset)))
+      (* (max steps 1)
+         c-basic-offset)))
+  (c-add-style "linux-tabs-only"
+               '("linux"
+                 (show-trailing-whitespace t)
+                 (indent-tabs-mode . t)
+                 (tab-width . 8)
+                 (c-offsets-alist (arglist-cont-nonempty
+                                   c-lineup-gcc-asm-reg
+                                   c-lineup-arglist-tabs-only))))
+
   ;; インデントスタイルを e style をデフォルトに
   (setq c-default-style
         '((c-mode . "e")
