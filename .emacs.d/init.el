28329ae2db6632287a1fe37e6b2d6b5aa78f5b43
Naruto TAKAHASHI <tnaruto@gmail.com>
2014-10-14 00:17:06 +0900

    enhanced helm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -403,10 +403,28 @@
   (global-set-key (kbd "C-c h") 'helm-mini)
   (custom-set-variables '(helm-ff-auto-update-initial-value nil))
   ;; helm commands
-  (global-set-key (kbd "M-x") 'helm-M-x)
-  ;; helm find files
-  (global-set-key (kbd "C-x C-f") 'helm-find-files)
-  
+  (define-key global-map (kbd "M-x")     'helm-M-x)
+  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+  (define-key global-map (kbd "C-x C-r") 'helm-recentf)
+  (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
+  (define-key global-map (kbd "C-c i")   'helm-imenu)
+  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
+
+  ;; Emulate `kill-line' in helm minibuffer
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
+  ;; For find-file etc.
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+  ;; For helm-find-files etc.
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+    "Execute command only if CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it))
+
   ;; helm-ag
   (require 'helm-ag)
   (setq helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
