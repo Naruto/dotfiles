c12166f870889e503d44630b39ca2f5dc09cd72b
Naruto TAKAHASHI <tnaruto@gmail.com>
2014-10-14 10:46:20 +0900

    add ac-clang-flags

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -164,7 +164,30 @@
     (setq ac-clang-complete-executable 
           (concat user-emacs-directory "bin/clang-complete"))
     (setq clang-completion-suppress-error 't)
-    (setq ac-clang-cflags (append '("-std=c++11") ac-clang-cflags))
+
+    ;; echo "" | g++ -v -x c++ -E -
+    (cond ((string= "gnu/linux" system-type)
+           (setq ac-clang-flags
+                 (mapcar (lambda (item)(concat "-I" item))
+                         (split-string
+                          "
+ /usr/include/c++/4.8
+ /usr/include/x86_64-linux-gnu/c++/4.8
+ /usr/include/c++/4.8/backward
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include
+ /usr/local/include
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed
+ /usr/include/x86_64-linux-gnu
+ /usr/include
+"
+                          )))
+           )
+          ((string= "darwin" system-type)
+           
+           )
+          (t t))
+
+    (setq ac-clang-cflags (append '("-std=c++11") ac-clang-cflags)) ;; c++11
     (ac-clang-launch-completion-process)
     )
 
