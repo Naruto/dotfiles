a2b56521f58a2f705560530bae3a914f4ec49df8
Naruto TAKAHASHI <tnaruto@gmail.com>
2014-11-28 02:19:56 +0900

    add ace-jump-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -626,6 +626,34 @@
 ;; )
 
 
+;; ace-jump-mode
+(when (file-exists-p
+       (expand-file-name (concat user-emacs-directory "public_repos/ace-jump-mode")))
+  (autoload
+    'ace-jump-mode
+    "ace-jump-mode"
+    "Emacs quick move minor mode"
+    t)
+  ;; you can select the key you prefer to
+  (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+
+
+  ;; 
+  ;; enable a more powerful jump back function from ace jump mode
+  ;;
+  (autoload
+    'ace-jump-mode-pop-mark
+    "ace-jump-mode"
+    "Ace jump back:-)"
+    t)
+  (eval-after-load "ace-jump-mode"
+    '(ace-jump-mode-enable-mark-sync))
+  (define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+  )
+
+
+
 ;; Create Header Guards with f12
 (global-set-key [f12] 
   		'(lambda () 
