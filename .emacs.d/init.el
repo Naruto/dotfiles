35f0c7414810e82c4e8148e1538ee5a20af72cc3
Naruto TAKAHASHI <tnaruto@gmail.com>
2023-10-02 01:42:22 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -341,18 +341,6 @@
 
 (provide 'init)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(blackout el-get hydra leaf-keywords leaf)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 ;; Local Variables:
 ;; indent-tabs-mode: nil
 ;; End:
