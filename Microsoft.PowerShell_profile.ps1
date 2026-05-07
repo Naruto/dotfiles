78c95f3ebe9b7eccfad955a56c68538ae1038747
Naruto TAKAHASHI <tnaruto@gmail.com>
2026-05-07 11:24:59 +0900

    Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,109 @@
+# Microsoft.PowerShell_profile.ps1
+
+# --- PSReadLine (Autosuggestions, Syntax Highlighting, History) ---
+# Ensure PSReadLine is updated: Install-Module PSReadLine -Force
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle InlineView
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -BellStyle None
+
+# --- Environment Variables ---
+$env:EDITOR = "code -w"
+$env:PAGER = "less"
+$env:BAT_PAGER = "less -R"
+
+# Path additions (Adjust as needed for your Windows setup)
+# $env:PATH = "$HOME\bin;$HOME\.cargo\bin;$env:PATH"
+
+# --- Aliases & Functions ---
+# rm -i equivalent
+function Remove-Item-Interactive { Remove-Item -Confirm $args }
+if (Get-Alias rm -ErrorAction SilentlyContinue) { Remove-Item Alias:rm -Force }
+Set-Alias rm Remove-Item-Interactive -Option AllScope
+
+# eza aliases
+function l { eza -lbF --git $args }
+function ll { eza -lbGF --git $args }
+function llm { eza -lbGd --git --sort=modified $args }
+function la { eza -lbhHigUmuSa --time-style=long-iso --git --color-scale $args }
+function lx { eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale $args }
+if (Get-Command eza -ErrorAction SilentlyContinue) {
+    Set-Alias ls eza
+}
+
+# other aliases
+Set-Alias cat bat
+Set-Alias top btm
+function rg { ripgrep.exe -p $args }
+$env:RIPGREP_CONFIG_PATH = "$HOME\.ripgreprc"
+
+# --- Tool Initializations ---
+
+# Starship
+if (Get-Command starship -ErrorAction SilentlyContinue) {
+    Invoke-Expression (&starship init powershell)
+}
+
+# Zoxide
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+}
+
+# Fzf
+if (Get-Command fzf -ErrorAction SilentlyContinue) {
+    Invoke-Expression (& { (fzf --powershell | Out-String) })
+    $env:FZF_DEFAULT_COMMAND = 'fd -HL --exclude ".git"'
+    $env:FZF_CTRL_T_COMMAND = 'fd -HL --exclude ".git" --type f'
+    $env:FZF_ALT_C_COMMAND = 'fd -HL --exclude ".git" --type d'
+    $env:FZF_DEFAULT_OPTS = '--layout=reverse --ansi --border --bind ctrl-v:page-down,alt-v:page-up,ctrl-k:kill-line'
+}
+
+# --- Custom Keybindings & Functions ---
+
+# ghq + fzf (Ctrl+])
+Set-PSReadLineKeyHandler -Key 'Ctrl+]' -ScriptBlock {
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    $target_dir = ghq list -p | fzf --query=$line
+    if ($target_dir) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::Replace(0, $line.Length, "cd `"$target_dir`"")
+        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+    }
+}
+
+# zoxide interactive (Ctrl+U)
+Set-PSReadLineKeyHandler -Key 'Ctrl+u' -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(0, 0, "zi ")
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+
+# yazi (Ctrl+O)
+function y {
+    $tmp = New-TemporaryFile
+    yazi --cwd-file="$tmp"
+    if (Test-Path $tmp) {
+        $cwd = Get-Content $tmp
+        if ($cwd -and $cwd -ne $PWD) {
+            Set-Location $cwd
+        }
+        Remove-Item $tmp
+    }
+}
+
+Set-PSReadLineKeyHandler -Key 'Ctrl+o' -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(0, 0, "y")
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+
+# lazygit
+function lg {
+    $env:LAZYGIT_NEW_DIR_FILE = "$HOME\.lazygit\newdir"
+    lazygit $args
+    if (Test-Path $env:LAZYGIT_NEW_DIR_FILE) {
+        Set-Location (Get-Content $env:LAZYGIT_NEW_DIR_FILE)
+        Remove-Item $env:LAZYGIT_NEW_DIR_FILE
+    }
+}
